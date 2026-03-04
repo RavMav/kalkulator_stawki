c266367604bdb87b58b3ccc124e5e1a1bb594add
RavMav <rafal.melanczuk@gmail.com>
2026-03-04 11:12:56 +0100

    dodano rozróżnienie na iOS/Android - dot. klawiatury i fokusa cd

--- main.py
+++ main.py
@@ -48,19 +48,22 @@ class Formularz_glowny(ft.Column):
         
         self.pole_input1 = ft.TextField(
             input_filter=ft.InputFilter(allow=True, regex_string=r"^\d*\.?\d*$", replacement_string=""),
-            visible=False, border_color=ft.Colors.GREEN_300, col={"sm": 12, "md": 4}, autofocus=True, keyboard_type=ft.KeyboardType.NUMBER,
+            visible=False, border_color=ft.Colors.GREEN_300, col={"sm": 12, "md": 4}, autofocus=True,
+            keyboard_type=ft.KeyboardType.TEXT if self.page and self.page.platform == ft.PagePlatform.IOS else ft.KeyboardType.NUMBER,
             on_submit=self.obsluga_enter, enable_suggestions=False
         )
         
         self.pole_input2 = ft.TextField(
             input_filter=ft.InputFilter(allow=True, regex_string=r"^\d*\.?\d*$", replacement_string=""),
-            visible=False, border_color=ft.Colors.GREEN_300, col={"sm": 12, "md": 4}, keyboard_type=ft.KeyboardType.NUMBER,
+            visible=False, border_color=ft.Colors.GREEN_300, col={"sm": 12, "md": 4},
+            keyboard_type=ft.KeyboardType.TEXT if self.page and self.page.platform == ft.PagePlatform.IOS else ft.KeyboardType.NUMBER,
             on_submit=self.obsluga_enter, enable_suggestions=False
         )
         
         self.pole_input3 = ft.TextField(
             input_filter=ft.InputFilter(allow=True, regex_string=r"^\d*\.?\d*$", replacement_string=""),
-            visible=False, border_color=ft.Colors.GREEN_300, col={"sm": 12, "md": 4}, keyboard_type=ft.KeyboardType.NUMBER,
+            visible=False, border_color=ft.Colors.GREEN_300, col={"sm": 12, "md": 4},
+            keyboard_type=ft.KeyboardType.TEXT if self.page and self.page.platform == ft.PagePlatform.IOS else ft.KeyboardType.NUMBER,
         )
         
         self.pole_akcyza = ft.TextField(label="Należności akcyza", visible=False, read_only=True, border_color=ft.Colors.GREEN_300, bgcolor=ft.Colors.GREEN_50, col={"sm": 12, "md": 4})
